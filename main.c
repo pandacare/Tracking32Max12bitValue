c0ac7f42304a080c54b1d88a53aae03118a3ee23
pandacare <chengliu.li@gmail.com>
2016-06-17 20:23:10 -0400

    main structure added

---
+++ main.c
@@ -0,0 +1,51 @@
+/**
+ * @file main.c
+ * @author Chengliu Li
+ * @date 17 June 2016
+ * @brief tracking 32 maximum values
+ *
+ *
+ *
+ *
+ */
+int heapArray[33];// min heap, i.e. the smallest value is in heap[1]
+int heapSize = 0;
+void heapFunction(int element){
+ 
+ //no return for now 
+  
+}
+
+void main (){
+
+//open file
+
+// while not the end of the file, get two 12bits data from the input file
+
+	// see if first value can be put into the 32 heap array?
+  		//check if the heap array is full?
+   			// in not full condition
+     				//insert the value to heap array
+  			// in full condition
+    				//check if the value is bigger than heapArray[1]
+       					//if the value is bigger
+          						//replace heap[1] with the new element
+
+	// see if second value can be put into the 32 heap array?
+  		//check if the heap array is full?
+   			// in not full condition
+     				//insert the value to heap array
+  			// in full condition
+    				//check if the value is bigger than heapArray[1]
+       					//if the value is bigger
+          						//replace heap[1] with the new element
+//reorder the heap with heap sort
+//output it to the output file
+
+//output the last 32 to the output file
+
+
+
+
+
+}
